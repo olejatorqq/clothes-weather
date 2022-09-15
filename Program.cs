870a2c768b0b50ce2efc381c02c6c5f5e74638aa
Oleg <oleg.orlowsky@gmail.com>
2022-09-15 20:53:38 +0300

    Added get your location for latitude and longitude

--- Program.cs
+++ Program.cs
@@ -1,2 +1,37 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Device.Location;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Request library
+using System.Net;
+using System.IO;
+using System.Text.Json;
+using ClothesWeather;
+using static ClothesWeather.GetIpClass;
+
+
+string pubIp =  new System.Net.WebClient().DownloadString("https://api.ipify.org");
+string? location = Get($"http://ip-api.com/json/{pubIp}");
+
+GetIpClass asd = JsonSerializer.Deserialize<GetIpClass>(location);
+
+Console.Write(asd.lat + " " + asd.lon);
+
+
+
+// Get method for find your Location
+string Get(string uri)
+{
+    HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+    request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+
+    using(HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+    using(Stream stream = response.GetResponseStream())
+    using(StreamReader reader = new StreamReader(stream))
+    {
+        return reader.ReadToEnd();
+    }
+}
