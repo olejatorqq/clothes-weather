57abafc41056af87b84e39c26c76f9fbe97b0a2a
Oleg <oleg.orlowsky@gmail.com>
2022-09-18 12:40:56 +0300

    Added Api service

--- Program.cs
+++ Program.cs
@@ -10,15 +10,35 @@ using System.Net;
 using System.IO;
 using System.Text.Json;
 using ClothesWeather;
-using static ClothesWeather.GetIpClass;
+
 
 
 string pubIp =  new System.Net.WebClient().DownloadString("https://api.ipify.org");
 string? location = Get($"http://ip-api.com/json/{pubIp}");
 
-GetIpClass asd = JsonSerializer.Deserialize<GetIpClass>(location);
+GetIpClass locationInfo = JsonSerializer.Deserialize<GetIpClass>(location);
 
-Console.Write(asd.lat + " " + asd.lon);
+//Console.Write(locationInfo.lat + " " + locationInfo.lon);
+
+
+// Using Api service for getting weather from your location (https://openweathermap.org/)
+
+string apiKey = "Paste your Api key here";
+
+var client = new HttpClient();
+var request = new HttpRequestMessage
+{
+    Method = HttpMethod.Get,
+    RequestUri = new Uri($"https://api.openweathermap.org/data/2.5/weather?lat={locationInfo.lat}&lon={locationInfo.lon}&appid={apiKey}"),
+};
+using (var response = await client.SendAsync(request))
+{
+    response.EnsureSuccessStatusCode();
+    var body = await response.Content.ReadAsStringAsync();
+    Console.WriteLine(body);
+}
+
+
 
 
 
